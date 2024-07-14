a522c6dac46398a749a4f057dc25e5722575e520
A. Ulaş Çankaya <aulascankaya@gmail.com>
2024-07-14 18:57:13 +0300

    Skills Component Created

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,7 +1,27 @@
+import { dataTR } from "../Datas/tr";
 export const Skills = () => {
-    return (
-        <div>
-            <h2>Skills</h2>
-            <p>HTML, CSS, JavaScript, React, Node.js, Express, MongoDB, PostgreSQL</p>
-        </div>
-    )}
+  const { items,title} = dataTR.skills;
+
+  return (
+    <div
+      id="skills"
+      className="text-left max-w-[80%] xl:max-w-6xl m-auto place-content-between"
+    >
+      <h2 className="font-bold text-black-heading dark:text-bluish-gray text-4xl mb-4">
+        {title}
+      </h2>
+      <div className="flex flex-col gap-x-4 lg:gap-x-8 lg:flex-row">
+        {items.map((skill, index) => (
+          // Beceri öğelerini haritalayarak her bir beceri öğesini gösteriyoruz.
+          <div key={index} className="pb-8 lg:pb-16">
+            <h3 className="text-dark-purple dark:text-dark-mode-name text-2xl mb-4 font-medium">
+              {skill.name}
+            </h3>
+            <p className="text-gray dark:text-white">{skill.description}</p>
+          </div>
+        ))}
+      </div>
+      <div className="h-[1px] bg-lilac mb-8 lg:mb-16"></div>
+    </div>
+  );
+};
